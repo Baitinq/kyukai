22efbd7a0e98a759d154bf389d7231e3438b9eaf
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-02-22 00:29:16 +0100

    SDL: Create simple testing window
    Works :^)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::pixels::Color;
+
+pub fn main() {
+    let sdl_context = sdl2::init().expect("Couldn't initialize the SDL2 context!");
+    let sdl_video = sdl_context
+        .video()
+        .expect("Couldn't get the SDL2 video subsystem!");
+
+    let window = sdl_video
+        .window("Kyukai", 10, 10)
+        .fullscreen()
+        .build()
+        .expect("Failed to build the SDL2 window!");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Couldn't get the SDL2 canvas from the window!");
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    loop {}
 }
